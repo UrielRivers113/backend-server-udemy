2277d7415820d262d7f8c836827d9142ba43ba12
Uriel Ríos <urielriosmartinez93@gmail.com>
2020-04-18 01:26:55 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 //Requires
 var express = require('express');
 var mongoos = require('mongoose');
-
+var bodyParser = require('body-parser')
 
 //Inicializar variables
 var app = express();
+
+
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //Conexión a la base de datos
 mongoos.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -13,12 +25,10 @@ mongoos.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =>
 })
 
 //Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'petición realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 //Escuchar peticiones
 app.listen(3000, () => {
